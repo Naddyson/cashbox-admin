9b571e5e92e27a7e4f70616f49e7763dc9da9c52
Naddyson <Naddyson777@gmail.com>
2018-06-27 12:08:19 +0300

    release v0.1

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { getSessions } from './api'
+import logo from './logo.svg';
+import _ from 'lodash';
+import './App.css';
+import 'react-datepicker/dist/react-datepicker.css';
+import { AppBar, Toolbar, Typography, Button } from 'material-ui';
+import HorizontalScroll from 'react-scroll-horizontal'
+import axios from 'axios'
+import Tables from './Tables';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+
+const serverURL = 'http://magic-cashbox-server.herokuapp.com';
+//const serverURL = 'http://localhost:3001';
+
+
+
+class App extends Component {
+    current_date;
+    constructor(props) {
+        super(props);
+        this.state = {
+            current_date: moment(),
+            all: 0,
+            active: 0,
+            sessions: [],
+        }
+    }
+
+    componentDidMount(){
+        this.getData();
+    }
+    getData = () => {
+        let date = this.state.current_date;
+        console.log('request')
+        axios.post(`${serverURL}/get-sessions`, { year: date.format('YYYY'), month: date.format('MM'), day: date.format('DD')})
+            .then( (res) => {
+                console.log(res);
+                this.setState({ sessions: res.data });
+                setTimeout(this.getData, 2000);
+            })
+            .catch( error => {
+                console.log(error.response);
+            })
+    };
+    handleChangeDate = (date) => {
+        this.setState({ current_date: date});
+    }
+
+
+    render() {
+        console.log(this.state.current_date);
+        let time = this.state.current_date.toLocaleString();
+
+    return (
+      <div className="App">
+          <AppBar position="static" color="default">
+              <Toolbar>
+                  <Button color="inherit">Julius & Company</Button>
+                  <DatePicker
+
+                      selected={this.state.current_date}
+                      onChange={this.handleChangeDate}
+                  />
+              </Toolbar>
+          </AppBar>
+
+           <Tables data={this.state.sessions}/>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Tables.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash'
+
+class Tables extends Component {
+    render() {
+        return (
+            <div style={{display: 'flex'}}>
+
+                {
+                    _.map(this.props.data, (item) => {
+                        return (
+                            <div style={{ flexDirection: 'row' }}>
+                                <table>
+                                    <tr>
+                                        <td className='table-head'>Name</td>
+                                        <td>{item.username}</td>
+                                    </tr>
+                                    <tr>
+                                        <td className='table-head'>City</td>
+                                        <td>{item.city}</td>
+                                    </tr>
+                                    <tr>
+                                        <td className='table-head'>Start</td>
+                                        <td>{item.startTime.substring(11,19)}</td>
+                                    </tr>
+                                    <tr>
+                                        <td className='table-head'>Finish</td>
+                                        <td>{
+                                            (()=> {
+                                                if (item.finishTime) {
+                                                    return item.finishTime.substring(11,19);
+                                                } else {
+                                                    return <div style={{ backgroundColor: 'green'}}>ACTIVE</div>
+                                                }
+                                            })()
+                                        }</td>
+                                    </tr>
+                                    <tr>
+                                        <td className='table-head'>$ Cash</td>
+                                        <td>{item.cash}</td>
+                                    </tr>
+                                    <th colSpan={2} style={{ width: '100%', textAlign: 'center', backgroundColor: 'gray'}}>
+                                        History
+                                    </th>
+                                    <tr>
+                                        <td>Time</td>
+                                        <td>Gain</td>
+                                    </tr>
+                                    {
+                                        _.map(item.history, (single) =>
+                                            <tr>
+                                                <td>{single.date.substring(11,19)}</td>
+                                                <td>{single.cashChange}</td>
+                                            </tr>
+                                        )
+                                    }
+                                </table>
+                            </div>
+                        )
+                    })
+                }
+
+            </div>
+        );
+    }
+}
+
+Tables.propTypes = {};
+
+export default Tables;
